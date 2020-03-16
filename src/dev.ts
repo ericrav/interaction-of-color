478f85036ee764b7a2c7926bf319947cd2fad02e
Eric Rabinowitz <ericrabinowitz@me.com>
2020-03-16 19:28:47 -0400

    Add command to copy colors to code

--- src/dev.ts
+++ src/dev.ts
@@ -4,9 +4,23 @@ import * as dat from 'dat.gui';
 // @ts-ignore
 import { sketch, colors } from './sketch.ts';
 
+const copyToClipboard = (text: string) => {
+  const input = document.createElement('textarea');
+  document.body.appendChild(input);
+  input.value = text;
+  input.select();
+  document.execCommand('copy');
+  document.body.removeChild(input);
+};
+
 const addGUI = (colors: string[] = []) => {
   const gui = new dat.GUI();
   colors.forEach((_, i) => gui.addColor(colors, String(i)));
+
+  const copy = () => {
+    copyToClipboard(colors.map(c => `  '${c}',`).join('\n'));
+  };
+  gui.add({ copy }, 'copy');
   return gui;
 };
 
