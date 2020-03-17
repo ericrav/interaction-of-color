c0107198d2785a5534b2f3f7ed1b806deb8c399c
Eric Rabinowitz <ericrabinowitz@me.com>
2020-03-17 11:01:22 -0400

    IV-5
    The diagonals are the same color

--- src/sketch.ts
+++ src/sketch.ts
@@ -1,26 +1,42 @@
 import { Sketch } from 'canvas-sketch';
 
 export const colors = [
-  '#ac2525',
-  '#e35b5b',
-  '#be3d3d',
-  '#7a93cf',
-  '#302b89',
+  '#9169ca',
+  '#3a215f',
+  '#b088eb',
+  '#2a585f',
+  '#fad251',
 ];
 
-export const sketch: Sketch = () => {
+export const sketch: Sketch = ({ context, width, height }) => {
+  const center = () => context.translate(width * 0.5, height * 0.5);
+
+  const rotatedRect = (color: string, x: number) => {
+    context.save();
+    const rotation = Math.PI * 0.92;
+    context.fillStyle = color;
+    center();
+    context.translate(width * x, 0);
+    context.rotate(rotation);
+    const w = width * 0.25;
+    const h = width * 0.75;
+    context.fillRect(-w*0.5, -h*0.5, w, h);
+    context.restore();
+  }
+
   return ({ context, width, height }) => {
-    context.fillStyle = colors[0];
-    context.fillRect(0, 0, width / 2, height);
     context.fillStyle = colors[1];
-    context.fillRect(width / 2, 0, width / 2, height);
+    context.fillRect(0, 0, width, height*0.5);
     context.fillStyle = colors[2];
-    const h = height / 8;
-    const w = h;
-    context.fillRect(width * 0.25, height * 0.5 - h / 2, width * 0.5, h);
+    context.fillRect(0, height*0.5, width, height*0.5);
+
+    rotatedRect(colors[0], -0.2);
+    rotatedRect(colors[0], 0.2);
+
     context.fillStyle = colors[3];
-    context.fillRect(width / 2, 0, w, height);
+    context.fillRect(0, height*0.25, width, height*0.1);
+
     context.fillStyle = colors[4];
-    context.fillRect(width / 2 - w, 0, w, height);
+    context.fillRect(0, height*0.35, width, height*0.4);
   };
 };
