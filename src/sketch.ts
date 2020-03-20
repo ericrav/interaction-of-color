78b79f110051d3b67d8eaeed9ddf4b2d8ad3280b
Eric Rabinowitz <ericrabinowitz@me.com>
2020-03-20 17:21:22 -0400

    Chapter V - tones of green

--- src/sketch.ts
+++ src/sketch.ts
@@ -1,14 +1,16 @@
 import { Sketch } from 'canvas-sketch';
 
 export const colors = [
-  '#042146',
-  '#6792dc',
-  '#36618e',
-  '#1d1959',
-  '#4868fc',
-  '#0F2DDC',
-  '#86add2',
-  '#183669',
+  '#396B24',
+  '#162a02',
+  '#64D87E',
+
+  '#22450F',
+
+  '#7a9b59',
+  '#20432F',
+  '#487D5E',
+  '#62AF7E',
 ];
 
 export const sketch: Sketch = ({ context, width, height }) => {
@@ -31,15 +33,15 @@ export const sketch: Sketch = ({ context, width, height }) => {
   context.fillRect(0, 0, width, height);
 
   return ({ context, width, height }) => {
-    rotatedRect(colors[0], 0.01, 0.2, 0.44);
-    rotatedRect(colors[1], 0.02, 0.28, 0.46);
-    rotatedRect(colors[2], -0.01, 0.36, 0.43);
-    rotatedRect(colors[3], 0.025, 0.44, 0.46);
-    rotatedRect(colors[4], -0.03, 0.52, 0.43);
+    rotatedRect(colors[0], -0.07, 0.2, 0.45);
+    rotatedRect(colors[1], -0.04, 0.26, 0.49);
+    rotatedRect(colors[2], -0.065, 0.33, 0.46);
 
-    rotatedRect(colors[5], 0.02, 0.62, 0.55);
+    rotatedRect(colors[4], -0.012, 0.56, 0.45);
+    rotatedRect(colors[5], 0.025, 0.63, 0.51);
+    rotatedRect(colors[6], -0.03, 0.72, 0.44);
+    rotatedRect(colors[7], -0.02, 0.8, 0.49);
 
-    rotatedRect(colors[7], -0.01, 0.8, 0.46);
-    rotatedRect(colors[6], 0.03, 0.73, 0.43);
+    rotatedRect(colors[3], 0.01, 0.45, 0.55);
   };
 };
