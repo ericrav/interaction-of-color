d3886a4f49c7830e3f4fc315c9c67ec368982331
Eric Rabinowitz <ericrabinowitz@me.com>
2020-03-22 13:13:48 -0400

    [VI] reversed grounds 2

--- src/sketch.ts
+++ src/sketch.ts
@@ -1,12 +1,19 @@
 import { Sketch } from 'canvas-sketch';
 
 export const colors = [
-  '#fa9e6c',
-  '#e66002',
-  '#f07838',
+  '#1a9687',
+  '#3d0082',
+  '#11487a',
 ];
 
 export const sketch: Sketch = ({ context, width, height }) => {
+  const circle = (x: number, r: number) => {
+    const cx = x * width;
+    const cy = 0.5 * height;
+    context.beginPath();
+    context.arc(cx, cy, r * width, 0, 2 * Math.PI);
+    context.stroke();
+  };
 
   const hLine = (x: number, y: number, w: number) => {
     context.moveTo(x * width, y * height);
@@ -24,18 +31,11 @@ export const sketch: Sketch = ({ context, width, height }) => {
     context.lineWidth = 0.008 * width;
     context.strokeStyle = colors[2];
 
-    hLine(0.05, 0.2, 0.4);
-    hLine(0.55, 0.2, 0.4);
-
-    hLine(0.05, 0.4, 0.2);
-    hLine(0.65, 0.4, 0.3);
-
-    hLine(0.05, 0.5, 0.9);
-
-    hLine(0.05, 0.6, 0.3);
-    hLine(0.75, 0.6, 0.2);
-
-    hLine(0.05, 0.8, 0.1);
-    hLine(0.85, 0.8, 0.1);
+    const radius = 0.18;
+    circle(0.25, radius);
+    circle(0.75, radius);
+    const x = 0.25 + radius;
+    const w = 0.5 - radius * 2;
+    hLine(x, 0.5, w);
   };
 };
