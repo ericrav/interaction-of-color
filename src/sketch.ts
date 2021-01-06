14c2f02e11254dca091849f72b7f86672db13948
Eric Rabinowitz <ericrabinowitz@me.com>
2021-01-05 20:16:11 -0500

    [IX] color mixture—illusion of transparence
    find a color between 2 colors that makes one or the other appear to show through the other

--- src/sketch.ts
+++ src/sketch.ts
@@ -1,45 +1,78 @@
 import { Sketch } from 'canvas-sketch';
 
 export const colors = [
-  '#471264',
-  '#f26d51',
-  '#894b65',
-  '#ac5858',
+  '#be5757',
+  '#5a754c',
+  '#5f423e',
 ];
 
 export const sketch: Sketch = ({ context, width, height }) => {
+  const rect = (x: number, y: number, size: number, aspectRatio: number) => {
+    context.fillRect(x * width, y * height, size * width, size * width / aspectRatio);
+  };
+
   return ({ context, width, height }) => {
     // backgrounds
-    const gradient = context.createLinearGradient(0, 0, width, 0);
-    gradient.addColorStop(0, colors[0]);
-    gradient.addColorStop(1, colors[1]);
-    context.fillStyle = gradient;
+    context.fillStyle = '#eee';
     context.fillRect(0, 0, width, height);
-
-    const s = height * 0.18;
-    const y = (height - s) * 0.5;
+    const fixedColor1 = '#D66E67';
+    const fixedColor2 = '#5D835D';
 
     {
-      const w = width * 0.4;
-      const h = s;
-      const x = (width - w) * 0.5;
-      context.fillStyle = 'white';
-      context.fillRect(x, y, w, h);
+      // example 1
+      const x1 = 0.02 * width;
+      const x2 = x1 + 0.16 * width;
+      const y1 = 0.06 * height;
+      const y2 = y1 + 0.2 * height;
+      const w = 0.3 * width;
+      const h = w / (16 / 9);
 
-      const gap = 16;
-      const w2 = w - gap * 2;
-      const h2 = (h - gap * 3) * 0.5;
-      context.fillStyle = colors[2];
-      context.fillRect(x + gap, y + gap, w2, h2);
-      context.fillStyle = colors[3];
-      context.fillRect(x + gap, y + h2 + gap * 2, w2, h2);
+      context.fillStyle = fixedColor1;
+      context.fillRect(x1, y1, w, h);
+      context.fillStyle = fixedColor2;
+      context.fillRect(x2, y2, w, h);
+
+      // overlapping region
+      context.fillStyle = colors[0];
+      context.fillRect(x2, y2, w - x2 + x1, h - y2 + y1);
     }
 
     {
+      // example 2
+      const x1 = 0.65 * width;
+      const x2 = x1 - 0.06 * width;
+      const y1 = 0.18 * height;
+      const y2 = y1 + 0.17 * height;
+      const w = 0.3 * width;
+      const h = w / (16 / 9);
+
+      context.fillStyle = fixedColor1;
+      context.fillRect(x1, y1, w, h);
+      context.fillStyle = fixedColor2;
+      context.fillRect(x2, y2, w, h);
+
+      // overlapping region
+      context.fillStyle = colors[1];
+      context.fillRect(x1, y2, w - x1 + x2, h - y2 + y1);
+    }
+
+    {
+      // example 3
+      const x1 = 0.1 * width;
+      const x2 = x1 + 0.06 * width;
+      const y1 = 0.72 * height;
+      const y2 = y1 + 0.09 * height;
+      const w = 0.7 * width;
+      const h = 0.15 * height;
+
+      context.fillStyle = fixedColor1;
+      context.fillRect(x1, y1, w, h);
+      context.fillStyle = fixedColor2;
+      context.fillRect(x2, y2, w, h);
+
+      // overlapping region
       context.fillStyle = colors[2];
-      context.fillRect(width * 0.1, y, s, s);
-      context.fillStyle = colors[3];
-      context.fillRect(width * 0.9 - s, y, s, s);
+      context.fillRect(x2, y2, w - x2 + x1, h - y2 + y1);
     }
   };
 };
